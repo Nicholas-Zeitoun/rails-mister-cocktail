91d2c674cb2a90b08900caace342296e1a9ed86c
NicholasZeitoun <nzeit8003@gmail.com>
2020-02-21 00:44:18 +1100

    added favicon and logo

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,7 +3,9 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      // console.log(window.scrollY);
+      // console.log(window.innerHeight);
+      if (window.scrollY >= 10) {
         navbar.classList.add('navbar-lewagon-white');
       } else {
         navbar.classList.remove('navbar-lewagon-white');
